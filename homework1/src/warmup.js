6e7a4b311287b54152d26db338573a3ccb22aac4
Donovan Moini <donovanmoini@gmail.com>
2018-08-30 20:54:49 -0700

    Added homework1

---
+++ homework1/src/warmup.js
@@ -0,0 +1,85 @@
+// Problem 1
+function change(cents) {
+  if (cents < 0) {
+    throw new RangeError('amount cannot be negative');
+  }
+  const denominations = [25, 10, 5, 1];
+  const results = [];
+  results.length = 3;
+  let remainingChange = cents;
+  denominations.forEach((denomination, index) => {
+    if (index < denominations.length - 1) {
+      results[index] = Math.floor(remainingChange / denomination);
+      remainingChange %= denomination;
+    } else {
+      results[index] = remainingChange;
+    }
+  });
+  return results;
+}
+
+function stripQuotes(s) {
+  // return s.split('').filter(character => character !== '"' && character !== '\'').join('');
+  return s.split(/['"]+/g).join('');
+}
+
+function scramble(s) {
+  const array = s.split('');
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array.join('');
+}
+
+function powers(base, limit, callback) {
+  let currentValue = 1;
+  while (currentValue <= limit) {
+    callback(currentValue);
+    currentValue *= base;
+  }
+  return currentValue;
+}
+
+function* powersGenerator(base, limit) {
+  let currentPower = 0;
+  while (base ** currentPower <= limit) {
+    yield base ** currentPower;
+    currentPower += 1;
+  }
+}
+
+// TODO: Problem 6
+
+
+// TODO: Problem 7
+
+
+// TODO: Problem 8
+
+
+// TODO: Problem 9
+
+
+// TODO: Problem 10
+
+module.exports = {
+  change,
+  stripQuotes,
+  scramble,
+  powers,
+  powersGenerator,
+  // say,
+  // interleave,
+  // cylinder,
+  // makeCryptoFunctions,
+  // randomName
+};
