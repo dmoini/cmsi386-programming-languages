2a81c721eb697ec3ee9636e519cf835b9c2de1da
Donovan Moini <donovanmoini@gmail.com>
2018-11-04 19:57:48 -0800

    Making progress

--- homework3/StreamPractice.java
+++ homework3/StreamPractice.java
@@ -2,10 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 import java.util.Optional;
 import java.io.IOException;
 import java.io.InputStreamReader;
+// import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 // import java.util.*;
@@ -18,7 +20,7 @@ public class StreamPractice {
         // Return a tree map whose keys are word lengths
         // and corresponding values are the number of words
         // in the reader with that length.
-        TreeMap<Integer, Long> tm = new TreeMap<Integer, Long>();
+        var tm = new TreeMap<Integer, Long>();
         reader.lines()
         .flatMap(line -> nonWord.splitAsStream(line.toLowerCase()))
         .filter(word -> !word.isEmpty())
@@ -41,17 +43,40 @@ public class StreamPractice {
             this.hits = Integer.parseInt(components[3]);
             this.average = (double)this.hits / (double)this.atBats;
         }
+
         @Override public String toString() {
           return this.name + " " + this.average;
         }
     }
 
-    public static int bestBatterByTeam(BufferedReader reader) {
-        // Return a map that records, for each team, the batter with
+    public static Map<String, Optional<Batter>> bestBatterByTeam(BufferedReader reader) {
+        // Return a map that records, for each team, the b with
         // the highest average over all batters that have at least
         // 100 at-bats.
-        // HashMap<String, Optional<Batter>> thing = new HashMap<String, Optional<Batter>>();
-        // return thing;
-        return 2;
+
+        var bestBatters = new HashMap<String, Optional<Batter>>();
+        reader.lines()
+        .map(b -> new Batter(b))
+        // .forEach(b -> bestBatters.put(b.team, Optional.empty()));
+        // .forEach(b -> System.out.println(b.team + " ---- player: " + b));
+        .filter(b -> b.atBats >= 100)
+        // .collect(Collectors.groupingBy(b->b.team, HashMap::new, Batter::getAtBats))
+        // .collect(Collectors.toMap(b -> b, b-> b.team))
+        // .forEach((b, team) -> { if (!bestBatters.containsKey(team) || bestBatters.get(team).average < b.average) { 
+        //                                 bestBatters.put(team, b); 
+        //                             }});
+        .forEach(b -> { System.out.println(b.team + ": " + b.atBats); });
+        // .forEach(b -> { if (!bestBatters.containsKey(b.team) || bestBatters.get(b.team).average < b.average) {
+        //     bestBatters.put(b.team, Optional(Batter));
+        // }});
+        System.out.println(bestBatters);
+        return bestBatters;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Batter teams = new Batter
+        var reader = new BufferedReader(new FileReader("batting_average_input.txt"));
+        var actual = bestBatterByTeam(reader);
+        System.out.println(actual);
     }
 }
