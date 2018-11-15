148d577195538f8e2104534aaad4a1ac11b0e524
Donovan Moini <donovanmoini@gmail.com>
2018-11-14 23:12:34 -0800

    Added tests. WIP

---
+++ homework4/queue_test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "queue.h"
+using namespace std;
+
+void testEmptyQueue() {
+    Queue<string> q;
+    assert(q.getSize() == 0);
+}
+
+void testEnqueue() {
+    Queue<string> q;
+    q.enqueue("Python");
+    q.enqueue("is");
+    q.enqueue("better");
+    assert(q.getSize() == 3);
+}
+
+void testDequeue() {
+    Queue<string> q;
+    q.enqueue("Python");
+    q.enqueue("is");
+    q.enqueue("better");
+    assert(q.getSize() == 3);
+    assert(q.dequeue() == "Python");
+    assert(q.dequeue() == "is");
+    assert(q.dequeue() == "better");
+}
+
+void testEnqueueAndDequeueAndSize() {
+    Queue<string> q;
+    assert(q.getSize() == 0);
+    q.enqueue("Python");
+    assert(q.getSize() == 1);
+    q.enqueue("is");
+    assert(q.getSize() == 2);
+    q.enqueue("better");
+    assert(q.getSize() == 3);
+    assert(q.dequeue() == "Python");
+    assert(q.getSize() == 2);
+    assert(q.dequeue() == "is");
+    assert(q.getSize() == 1);
+    assert(q.dequeue() == "better");
+    assert(q.getSize() == 0);
+}
+
+void testDequeueOnEmptyQueue() {
+    Queue<string> q;
+    try {
+        q.dequeue();
+        assert(false);
+    } catch (const underflow_error& ue) {
+        assert(true);
+    } catch (...) {
+        assert(false);
+    }
+}
+
+int main() {
+    testEmptyQueue();
+    testEnqueue();
+    testDequeue();
+    testEnqueueAndDequeueAndSize();
+    testDequeueOnEmptyQueue();
+    cout << "All tests passed :)\n";
+}
