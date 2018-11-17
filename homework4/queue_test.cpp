66844700e506796f089a7bb7c158651199f74164
Donovan Moini <donovanmoini@gmail.com>
2018-11-17 14:02:24 -0800

    Used Toal's tests

--- homework4/queue_test.cpp
+++ homework4/queue_test.cpp
@@ -2,78 +2,77 @@
 #include <string>
 #include <cassert>
 #include "queue.h"
+
 using namespace std;
 
-int destructorCalls = 0;
-void testEmptyQueue() {
+void test_empty_queue_has_zero_size() {
     Queue<string> q;
-    assert(q.getSize() == 0);
+    assert(q.get_size() == 0);
 }
 
-void testEnqueue() {
+void test_some_insertions_and_deletions() {
     Queue<string> q;
-    q.enqueue("Python");
-    q.enqueue("is");
-    q.enqueue("better");
-    assert(q.getSize() == 3);
+    q.enqueue("one");
+    assert(q.get_size() == 1);
+    q.enqueue("two");
+    q.enqueue("three");
+    assert(q.get_size() == 3);
+    assert(q.dequeue() == "one");
+    assert(q.get_size() == 2);
+    assert(q.dequeue() == "two");
+    assert(q.dequeue() == "three");
+    q.enqueue("four");
+    assert(q.get_size() == 1);
 }
 
-void testDequeue() {
-    Queue<string> q;
-    q.enqueue("Python");
-    q.enqueue("is");
-    q.enqueue("better");
-    assert(q.getSize() == 3);
-    assert(q.dequeue() == "Python");
-    assert(q.dequeue() == "is");
-    assert(q.dequeue() == "better");
+void test_no_copies() {
+    Queue<int> p;
+    Queue<int> q;
+    // No way to test this at run time, but uncomment the following lines
+    // and look for compile time errors.
+    // p = q;
+    // Queue<int> r(p);
 }
 
-void testEnqueueAndDequeueAndSize() {
-    Queue<string> q;
-    assert(q.getSize() == 0);
-    q.enqueue("Python");
-    assert(q.getSize() == 1);
-    q.enqueue("is");
-    assert(q.getSize() == 2);
-    q.enqueue("better");
-    assert(q.getSize() == 3);
-    assert(q.dequeue() == "Python");
-    assert(q.getSize() == 2);
-    assert(q.dequeue() == "is");
-    assert(q.getSize() == 1);
-    assert(q.dequeue() == "better");
-    assert(q.getSize() == 0);
+Queue<int> one_two_three() {
+    Queue<int> q;
+    for (int i = 1; i <= 3; i++) q.enqueue(i);
+    return q;
 }
 
-void testDequeueOnEmptyQueue() {
-    Queue<string> q;
+void test_moves() {
+    // Assignment of a temporary is a move
+    Queue<int> p = Queue<int>();
+
+    // Construction via a function return call is a move
+    Queue<int> q = one_two_three();
+    assert(q.get_size() == 3);
+    assert(q.dequeue() == 1);
+
+    // Test move assignment
+    q = one_two_three();
+    assert(q.get_size() == 3);
+    assert(q.dequeue() == 1);
+}
+
+void test_dequeue_from_empty_queue_throws_underflow_error() {
+    Queue<bool> q;
     try {
         q.dequeue();
         assert(false);
     } catch (const underflow_error& ue) {
         assert(true);
     } catch (...) {
+        // Caught the wrong exception
         assert(false);
     }
 }
 
-void testDestructor() {
-    Queue<string> test1;
-    Queue<string> test2;
-    Queue<string> test3;
-}
-
-void testMoveOperator() {
-
-}
 int main() {
-    testDestructor();
-    assert(destructorCalls == 3);
-    testEmptyQueue();
-    testEnqueue();
-    testDequeue();
-    testEnqueueAndDequeueAndSize();
-    testDequeueOnEmptyQueue();
-    cout << "All tests passed :)\n";
+    test_empty_queue_has_zero_size();
+    test_some_insertions_and_deletions();
+    test_dequeue_from_empty_queue_throws_underflow_error();
+    test_no_copies();
+    test_moves();
+    cout << "All tests passed\n";
 }
