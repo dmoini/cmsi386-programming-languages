3115bf12db63ac68e8f78748d15cf2cce7930f9f
Donovan Moini <donovanmoini@gmail.com>
2018-12-02 18:49:00 -0800

    Finished fUOLF, socoo, and powers

--- homework5/haskell/Warmup.hs
+++ homework5/haskell/Warmup.hs
@@ -1,8 +1,16 @@
 module Warmup
     (change
-    , stripQuotes)
+    , stripQuotes
+    , firstUppercasedOverLengthFive
+    , powers
+    , sumOfCubesOfOdds
+    , swapAdjacents)
     where
+        -- ^^^ TODO: add to module data type for 3-D shapes
 
+import Data.Char
+
+change :: Int -> Either String (Int, Int, Int, Int)
 change amount =
     if amount < 0 then
         Left "amount cannot be negative"
@@ -13,4 +21,30 @@ change amount =
             (nickels, pennies) = afterDimes `divMod` 5
         in Right (quarters, dimes, nickels, pennies)
 
+stripQuotes :: String -> String
 stripQuotes s = filter (\c -> c /= '\'' && c /= '"') s
+
+-- A function that returns the uppercased version of the fist string 
+-- in a list that has a length greater than 5, wrapped in a Maybe, 
+-- since there might not be any strings greater than 5 in the list.
+firstUppercasedOverLengthFive :: [String] -> Maybe String
+firstUppercasedOverLengthFive [] = Nothing
+firstUppercasedOverLengthFive (s:strings) = 
+    if length s > 5 then
+        Just (map toUpper s)        
+    else
+        firstUppercasedOverLengthFive strings
+
+powers :: Num a => a -> [a]
+powers num = map (num^) [0..]
+
+sumOfCubesOfOdds :: [Int] -> Int
+sumOfCubesOfOdds [] = 0
+sumOfCubesOfOdds (n:nums) = 
+    if n `mod` 2 /= 0 then
+        n * n * n + sumOfCubesOfOdds nums
+    else
+        sumOfCubesOfOdds nums
+
+-- TODO
+swapAdjacents list = 0
